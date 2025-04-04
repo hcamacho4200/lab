6540d4842dbf4d798b248274feb2e11b8b8dc8cb
hcamacho4200 <you@example.comhenry.f.camacho@gmail.com>
2025-04-04 17:03:21 +0000

    add support for ping into phpipam, added uptime-kuma

--- start_containers.sh
+++ start_containers.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 cd ~/lab/infrastructure/utility/containers/homepage
+podman-compose build
 podman-compose up -d
 sudo chown -R ubuntu:ubuntu volumes/config
 
 cd ~/lab/infrastructure/utility/containers/phpipam
+podman-compose build
 podman-compose up -d
 
 cd ~/lab/infrastructure/utility/containers/portainer
+podman-compose build
+podman-compose up -d
+
+cd ~/lab/infrastructure/utility/containers/uptime-kuma
+podman-compose build
 podman-compose up -d
--- stop_containers.sh
+++ stop_containers.sh
@@ -2,10 +2,11 @@
 cd ~/lab/infrastructure/utility/containers/homepage
 podman-compose down -t 2
 
-
 cd ~/lab/infrastructure/utility/containers/phpipam
 podman-compose down -t 2
 
 cd ~/lab/infrastructure/utility/containers/portainer
 podman-compose down -t 2
 
+cd ~/lab/infrastructure/utility/containers/uptime-kuma
+podman-compose down -t 2
